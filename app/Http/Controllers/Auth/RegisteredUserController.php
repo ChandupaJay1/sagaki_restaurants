8361d76a3d050b319a71dd8805e5fa4faecba363
M G Pathum Kaushallaya <mgpdesaman@gmail.com>
2026-08-13 21:50:43 +0530

    Update Login Page

---
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Models\User;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * Display the registration view.
+     */
+    public function create(): View
+    {
+        return view('auth.register', [
+            'status' => session('status'),
+        ]);
+    }
+
+    /**
+     * Handle an incoming registration request.
+     */
+    public function store(RegisterRequest $request): RedirectResponse
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        Auth::login($user);
+
+        $request->session()->regenerate();
+
+        return redirect()->intended(route('dashboard', absolute: false));
+    }
+}
